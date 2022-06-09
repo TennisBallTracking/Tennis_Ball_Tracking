d69a44f03a7fa6a7eb3dc0d5a98e3a3969759945
Sports_Analytics <106950169+TennisBallTracking@users.noreply.github.com>
2022-06-09 17:40:43 +0800

    Update single_frame_inference.py

--- src/single_frame_inference.py
+++ src/single_frame_inference.py
@@ -16,6 +16,7 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
 
+# Calculate distance
 def get_dist(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
